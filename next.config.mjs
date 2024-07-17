9d26cc9d47b3dd447e7879e4aead773dca93466a
oldboydevelop <dev@oldboybarbershop.com>
2024-07-17 15:29:01 +0200

    deploy17

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   images: {
-    domains: ["cdn.dummyjson.com"], // Добавляем домен вашего изображения сюда
+    domains: ["cdn.dummyjson.com"],
+    loader: "default",
   },
 };
 
